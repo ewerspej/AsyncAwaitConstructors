a3dd1cf8ea349088bf9883dde732b7b661b827b7
Julian Ewers <ewerspej@tcd.ie>
2023-07-31 10:53:43 +0200

    setup project

---
+++ AsyncAwaitConstructors/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AsyncAwaitConstructors
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
